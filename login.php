b6a3dcec49357b0e541ee6ccc8fd57ff18b0b320
russcuevas <russelcuevas0@gmail.com>
2024-01-05 23:27:58 +0800

    first-day

---
+++ login.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Student Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/login/logo-kll.jpg" />
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- end of bootstrap -->
+    <!--page level css -->
+    <link type="text/css" href="vendors/themify/css/themify-icons.css" rel="stylesheet" />
+    <link href="vendors/iCheck/css/all.css" rel="stylesheet">
+    <link href="vendors/bootstrapvalidator/css/bootstrapValidator.min.css" rel="stylesheet" />
+    <link href="assets/css/login.css?v=2" rel="stylesheet">
+    <link href="assets/css/app/evsu-theme.css?v=1" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!--end page level css-->
+</head>
+
+<body id="sign-in">
+
+    <div class="preloader">
+        <div class="loader_img"><img src="/assets/img/loader.gif" alt="loading..." height="64" width="64"></div>
+    </div>
+
+    <div class="flex-container">
+        <div class="container">
+            <div class="row row-inner-container">
+                <div class="col-md-12">
+                    <div class="row my-form-row">
+                        <div class="col-md-6 my-col">
+                            <div class="logo-container">
+                                <img class="img-logo" src="images/login/logo-kll.jpg">
+                            </div>
+                        </div>
+                        <div class="col-md-6 my-col">
+                            <div class="my-form-container">
+
+                                <div class="my-form-inner-container">
+                                    <div class="panel-header">
+                                        <h2 class="text-center">
+                                            KLL STUDENT PORTAL
+                                        </h2>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+
+                                            <div class="col-xs-12">
+                                                <h3 style="font-weight: bold;margin-bottom: 20px;">Sign In</h3>
+                                                <form action="" id="" method="post" class="">
+                                                    <div class="form-group">
+                                                        <label for="email" class="sr-only"> Email</label>
+                                                        <input required type="text" class="form-control  form-control-lg input-lg" id="student_id" name="email" placeholder="Email">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="password" class="sr-only">Password</label>
+                                                        <input required type="password" class="form-control form-control-lg input-lg" id="password" name="password" placeholder="Password">
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <button class="btn btn-primary btn-block btn-lg" type="submit">Login</button>
+                                                    </div>
+                                                    <div class="clearfix"></div>
+                                                </form>
+                                            </div>
+                                            <div class="col-xs-12">
+                                                <br><br>
+                                                <p class="text-center">By using this service, you understood and agree to the <a href="" target="_blank">KLL Terms of Use and Privacy Statement</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p style="margin: 10px 0px"><a href="home.php" style="color: white !important; font-weight: 900;"><small>&larr; Back to Homepage</small></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- global js -->
+    <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- end of global js -->
+    <!-- page level js -->
+    <script type="text/javascript" src="vendors/iCheck/js/icheck.js"></script>
+    <script src="vendors/bootstrapvalidator/js/bootstrapValidator.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="assets/js/custom_js/login.js?v=3"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-182226591-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-182226591-1');
+    </script>
+</body>
+
+</html>
